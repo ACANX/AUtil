e351b4426184f3b4f64f6a3e44c0f73ee6c35c1e
ACANX <245818784@qq.com>
2020-04-07 21:16:20 +0800

    BlogServer功能增强： 1.博客登录及注册用户增加验证码验证过程，初步实现了防御机器人恶意注册功能； 2.增加/.loginPage/.loginProcessingUrl()/.successForwardUrl("/User/Home") /.failureUrl("/login-error")的说明 3.增加部分页面的跳转过程，替换过去提交表单后显示json的不友好显示界面方式； 4.增加User/域，用户登录之后进入user页面，然后开展其他操作。 5.增加UserVo对象； 6.优化博文编辑页面，初步实现一个界面友好且普通用户可接受的界面； 7.修改prod环境的日志级别，便于本地开发时调试程序；

--- src/main/java/com/acanx/utils/LocalDateTimeUtil.java
+++ src/main/java/com/acanx/utils/LocalDateTimeUtil.java
@@ -1,5 +1,8 @@
 package com.acanx.utils;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
 /**
  * ACANX-Demo / com.acanx.utils / LocalDateTimeUtil.java
  * 文件由 ACANX 创建于 2019/7/26 - 17:15
@@ -8,9 +11,30 @@ package com.acanx.utils;
  *
  * @author ACANX
  * @version 0.0.1.6
- *  Date 2019/7/26  17:15
+ * Date 2019/7/26  17:15
  * @since 0.0.1-SNAPSHOT
  */
 public class LocalDateTimeUtil {
+
+
+    /**
+     * LocalDateTime转Date
+     * @param localDateTime
+     * @return
+     */
+    public static Date LocalDateTime2Date(LocalDateTime localDateTime) {
+        return null;
+    }
+
+
+    /**
+     * Date转LocalDateTime
+     * @param date
+     * @return
+     */
+    public static LocalDateTime Date2LocalDateTime(Date date) {
+        return null;
+    }
+
 
 }
