c61388c8db8156fc8409f9b09607c987d183eaab
ACANX <245818784@qq.com>
2020-04-12 20:34:46 +0800

    [INFO] ------------------------------------------------------------------------ [INFO] Reactor Summary for ACANX-Demo 0.0.1.1: [INFO] [INFO] ACANX-Demo ......................................... SUCCESS [  0.446 s] [INFO] ACANX-Common ....................................... SUCCESS [  0.017 s] [INFO] ACANX-Common-Core .................................. SUCCESS [  2.438 s] [INFO] ACANX-Common-Web ................................... SUCCESS [  0.204 s] [INFO] ACANX-SpringBoot-Color-Logger-Starter .............. SUCCESS [  0.740 s] [INFO] ACANX-Demo-Dependencies ............................ SUCCESS [  0.014 s] [INFO] ACANX-Services ..................................... SUCCESS [  0.013 s] [INFO] ACANX-CMS-Server ................................... SUCCESS [  0.722 s] [INFO] ACANX-API-Server ................................... SUCCESS [  0.310 s] [INFO] ACANX-Blog-Service ................................. SUCCESS [ 23.849 s] [INFO] ACANX-Family-Hub-Server ............................ SUCCESS [  0.284 s] [INFO] ACANX-ElasticSearch-Service-Demo ................... SUCCESS [  3.716 s] [INFO] ACANX-Dev-Demo ..................................... SUCCESS [  0.570 s] [INFO] ACANX-SSO-Server ................................... SUCCESS [  0.401 s] [INFO] ACANX-Dev-Server ................................... SUCCESS [  2.220 s] [INFO] ACANX-Baidu-API-Service ............................ SUCCESS [  0.371 s] [INFO] ACANX-KPVICS ....................................... SUCCESS [  0.239 s] [INFO] ACANX-MassiveData-Demo ............................. SUCCESS [  0.012 s] [INFO] ACANX-MassiveData-Platform ......................... SUCCESS [  0.012 s] [INFO] ACANX-Platform ..................................... SUCCESS [  0.012 s] [INFO] ACANX-SpringCloud-Platform ......................... SUCCESS [  0.011 s] [INFO] ACANX-MSOA-Platform ................................ SUCCESS [  0.014 s] [INFO] ACANX-MSOA-Gateway ................................. SUCCESS [  2.972 s] [INFO] ACANX-MSOA-Admin ................................... SUCCESS [  2.407 s] [INFO] ACANX-MSOA-Auth .................................... SUCCESS [  0.633 s] [INFO] ACANX-MSOA-UPMS .................................... SUCCESS [  0.524 s] [INFO] ACANX-Spring-Cloud-Starter ......................... SUCCESS [  0.042 s] [INFO] spring-boot-starter-acanx-blog ..................... SUCCESS [  0.020 s] [INFO] ------------------------------------------------------------------------ [INFO] BUILD SUCCESS [INFO] ------------------------------------------------------------------------ [INFO] Total time:  45.653 s [INFO] Finished at: 2020-04-12T20:32:49+08:00 [INFO] ------------------------------------------------------------------------

---
+++ src/test/java/com/acanx/utils/image/CaptchaUtilTest.java
@@ -0,0 +1,59 @@
+package com.acanx.utils.image;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.theories.suppliers.TestedOn;
+
+import javax.imageio.ImageIO;
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.Map;
+
+/**
+ * CaptchaUtil Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ * @since <pre>4月 5, 2020</pre>
+ */
+public class CaptchaUtilTest {
+
+    @Before
+    public void before() throws Exception {
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    /**
+     * Method: generateCodeAndPic()
+     */
+    @Test
+    public void testGenerateCodeAndPic() throws Exception {
+
+        Long totalCost=0L;
+        for (int i = 0; i < 200; i++) {
+            //创建文件输出流对象
+            Long start = System.currentTimeMillis();
+            String path="D://" + start + ".jpg";
+            File file = new File(path);
+            if(!file.exists()){
+                file.createNewFile();
+            }
+            OutputStream out = new FileOutputStream(file);
+            Map<String, Object> map = CaptchaUtil.generateCodeAndPic();
+            ImageIO.write((RenderedImage) map.get("codePic"), "jpeg", out);
+            Long stop = System.currentTimeMillis();
+            Long cost = (stop - start);
+            System.out.println("验证码的值为：" + map.get("code") + " 生成耗时：" + cost + "ms");
+            totalCost=totalCost+cost;
+        }
+        System.out.println("平均耗时："+(totalCost/200)+"ms");
+    }
+
+
+} 
