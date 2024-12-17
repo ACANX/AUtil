bea9c91f35bf9823e5458aa0a64b0461431cb875
ACANX <245818784@qq.com>
2024-12-17 11:07:26 +0800

    添加对象判空公共方法

--- src/main/java/com/acanx/utils/incubator/ObjectUtil.java
+++ src/main/java/com/acanx/utils/incubator/ObjectUtil.java
@@ -3,11 +3,26 @@ package com.acanx.utils.incubator;
 import com.acanx.annotation.Alpha;
 
 /**
- *  ObjectUtil
+ * ObjectUtil
  *
  * @since 0.0.1.10
  */
 @Alpha
 public class ObjectUtil {
+
+
+    /**
+     * 对象判空
+     *
+     * @param obj
+     * @return   返回的判断结果: true - 为空; false - 非空
+     */
+    boolean isNull(Object obj) {
+        if (obj == null) {
+            return true;
+        }
+        return false;
+    }
+
 
 }
