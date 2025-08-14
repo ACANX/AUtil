1f254da06cb20dea668a5942c1cec39abe0c34d7
ACANX <245818784@qq.com>
2025-08-14 09:40:55 +0800

    feat: 增加打印工具类PrintUtil

---
+++ autil-incubator/incubator-core/src/test/java/com/acanx/util/PrintUtilTest.java
@@ -0,0 +1,13 @@
+package com.acanx.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrintUtilTest {
+
+    @Test
+    void printCfRuntimeEnvInfo() {
+        PrintUtil.printCfRuntimeEnvInfo();
+    }
+}
