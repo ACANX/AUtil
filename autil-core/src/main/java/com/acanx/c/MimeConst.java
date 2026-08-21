9a5bc43da71388ee976caeb446e3a9b969249573
ACANX <245818784@qq.com>
2026-08-21 10:22:00 +0800

    feat(http): 补充常用MIME类型常量(CONTENT_TYPE值)

--- autil-core/src/main/java/com/acanx/c/MimeConst.java
+++ autil-core/src/main/java/com/acanx/c/MimeConst.java
@@ -12,6 +12,23 @@ public class MimeConst {
     public static final String JSON = "application/json";
     /**  X_WWW_FORM_URLENCODED  */
     public static final String X_WWW_FORM_URLENCODED = "application/x-www-form-urlencoded";
-
+    /**  XML  */
+    public static final String XML = "application/xml";
+    /**  TEXT_XML  */
+    public static final String TEXT_XML = "text/xml";
+    /**  TEXT_PLAIN  */
+    public static final String TEXT_PLAIN = "text/plain";
+    /**  TEXT_HTML  */
+    public static final String TEXT_HTML = "text/html";
+    /**  TEXT_CSV  */
+    public static final String TEXT_CSV = "text/csv";
+    /**  MULTIPART_FORM_DATA  */
+    public static final String MULTIPART_FORM_DATA = "multipart/form-data";
+    /**  OCTET_STREAM  */
+    public static final String OCTET_STREAM = "application/octet-stream";
+    /**  JAVASCRIPT  */
+    public static final String JAVASCRIPT = "application/javascript";
+    /**  PDF  */
+    public static final String PDF = "application/pdf";
 
 }
